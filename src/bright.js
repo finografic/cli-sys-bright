4c9246c875a6c6ade40d5683ebf1adb74220d50c
Justin <justin.blair.rankin@gmail.com>
2021-02-23 07:48:45 +0100

    parsing functions moved to utils.js

--- src/bright.js
+++ src/bright.js
@@ -2,8 +2,8 @@ import chalk from 'chalk';
 import sudo from 'sudo-prompt';
 import updateNotifier from 'update-notifier';
 import { brightness, backlight, sysFileBrightness, cli, flags } from './config';
-import { getNumericValues } from './utils';
-import { log, logValueTooLow, logValueTooHigh, logInferredDefaults, logInvalidInput, logSummary } from './logging';
+import { getParsedFlags, getParsedInput } from './utils';
+import { log, logSummary } from './logging';
 
 const pkg = require('../package.json');
 
@@ -14,95 +14,6 @@ updateNotifier({ pkg }).notify();
 export async function bright() {
   const inputArg = cli.input[0]; // ONLY ACCEPT *SINGLE* ARGUMENT. USE FIRST ARG, IGNORE REST.
   const inputFlags = Object.keys(cli.flags).length ? cli.flags : null;
-
-  const getFlagValue = (flagKey, flagValue, flagTarget) => {
-    switch (true) {
-      case isNaN(flagValue) && Object.keys(flagTarget).includes(flagValue):
-        return {
-          [flagKey]: flagTarget[flagValue],
-        };
-      case isNaN(flagValue) && !Object.keys(flagTarget).includes(flagValue):
-        log(chalk.red(`Flag value ${chalk.bold(flagValue)} is invalid`));
-        logInferredDefaults();
-        return {
-          [flagKey]: flagTarget.default,
-        };
-      case flagValue < flagTarget.min:
-        logValueTooLow(flagKey);
-        return {
-          [flagKey]: flagTarget.min,
-        };
-      case flagValue > flagTarget.max:
-        logValueTooHigh(flagKey);
-        return {
-          [flagKey]: flagTarget.max,
-        };
-      default:
-        return {
-          [flagKey]: flagValue,
-        };
-    }
-  };
-
-  /**
-   * Determine if user input is a predefined default, numeric, or other.
-   * @param {string} [input] - User input always enters as type string.
-   * @param {object} [inputFlags] - User supplied flags.
-   */
-  const getParsedFlags = (inputFlags) => {
-    // HANDLE inputFlags ===================================================== //
-
-    const validFlags = Object.keys(flags);
-    const inputFlagsValid = {};
-
-    if (inputFlags) {
-      for (const flag of Object.keys(inputFlags)) {
-        let flagValue = inputFlags[flag];
-        const isValidFlag = validFlags.includes(flag);
-        if (isValidFlag) {
-          if (flag === 'brightness') flagValue = getFlagValue(flag, flagValue, brightness);
-          if (flag === 'backlight') flagValue = getFlagValue(flag, flagValue, backlight);
-          inputFlagsValid[flag] = Number(flagValue[flag]);
-        }
-      }
-    }
-
-    return {
-      brightness: inputFlagsValid.brightness || brightness.default,
-      backlight: inputFlagsValid.backlight || backlight.default,
-    };
-  };
-
-  /**
-   * Determine if user input is a predefined default, numeric, or other.
-   * @param {string} [input] - User input always enters as type string.
-   * @param {object} [inputFlags] - User supplied flags.
-   */
-  const getParsedInput = (input) => {
-    // HANDLE input ===================================================== //
-
-    if ([
-      'min',
-      'max',
-      'low',
-      'high', 
-    ].includes(input)) {
-      return {
-        brightness: brightness[input],
-        backlight: backlight[input],
-      };
-    } else if (!isNaN(Number(input))) {
-      return getNumericValues(Number(input));
-    } else {
-      if (input !== undefined && input !== 'default') logInvalidInput();
-      logInferredDefaults();
-      return {
-        brightness: brightness.default,
-        backlight: backlight.default,
-        inferredSetting: 'backlight',
-      };
-    }
-  };
 
   // ============================================================== //
   // PARSE ARG AND/OR FLAG INPUT VALUES
